39d5a6fabbae1901e83954fed78df7a6d9e4a08c
Luis Boullosa <l.f.valdes.villarreal.boullosa@student.rug.nl>
2019-05-07 18:52:23 +0200

    Added function for proper dividers in R and its tests.
    Signed-off-by: Luis Boullosa <l.f.valdes.villarreal.boullosa@student.rug.nl>

--- tests/testthat/test-get_proper_divisors_R.R
+++ tests/testthat/test-get_proper_divisors_R.R
@@ -1,9 +1,10 @@
-context("get_proper_divisors")
+context("get_proper_divisors_R")
 
+## TODO: Rename context
+## TODO: Add more tests
 
 test_that("use", {
 
- skip("calc_squared: use")
  expect_equal(get_proper_divisors(1), c())
  expect_equal(get_proper_divisors(2), c(1))
  expect_equal(get_proper_divisors(3), c(1))
@@ -12,6 +13,6 @@ test_that("use", {
  expect_equal(get_proper_divisors(6), c(1, 2, 3))
  expect_equal(get_proper_divisors(7), c(1))
  expect_equal(get_proper_divisors(8), c(1, 2, 4))
- expect_equal(get_proper_divisors(9), c(1, 2, 3))
+ expect_equal(get_proper_divisors(9), c(1, 3))
 
 })
