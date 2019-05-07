3adae02e4078c39836e4d273db74379870da6a2d
Luis Boullosa <l.f.valdes.villarreal.boullosa@student.rug.nl>
2019-05-07 17:18:15 +0200

    Added sum function.
    Signed-off-by: Luis Boullosa <l.f.valdes.villarreal.boullosa@student.rug.nl>

--- src/calc_sum_cpp.cpp
+++ src/calc_sum_cpp.cpp
@@ -1,7 +1,13 @@
 #include "calc_sum_cpp.h"
 
-double calc_sum_cpp(const std::vector<double>&)
+
+double calc_sum_cpp(const std::vector<double>& values)
 {
   //A stub
-  return 0.0;
+	double dSum = 0;
+	for (size_t i = 0; i < size(v); i++)
+	{
+		dSum += v[i];
+	}
+  return dSum;
 }
